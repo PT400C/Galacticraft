dc92cf732fecf95f90f8d73b84295eee4f6e54d9
glowredman <fredcraft00@gmail.com>
2022-11-23 23:45:20 +0100

    add missing `@Overwrite`

--- src/main/java/micdoodle8/mods/galacticraft/core/mixins/minecraft/WorldMixin.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/mixins/minecraft/WorldMixin.java
@@ -3,10 +3,12 @@ package micdoodle8.mods.galacticraft.core.mixins.minecraft;
 import micdoodle8.mods.galacticraft.core.util.WorldUtil;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(World.class)
 public abstract class WorldMixin {
 
+    @Overwrite
     public float getRainStrength(float partialTicks) {
         return WorldUtil.getRainStrength((World) (Object) this, partialTicks);
     }
