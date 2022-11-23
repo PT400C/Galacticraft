dc7d08bc1af2fcfde26786ee01ea6ce3f2e14f3f
glowredman <fredcraft00@gmail.com>
2022-11-23 23:30:35 +0100

    let's see if this works...

--- src/main/java/micdoodle8/mods/galacticraft/core/mixins/minecraft/EntityMixin.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/mixins/minecraft/EntityMixin.java
@@ -3,10 +3,12 @@ package micdoodle8.mods.galacticraft.core.mixins.minecraft;
 import micdoodle8.mods.galacticraft.core.util.WorldUtil;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
 
+    @Overwrite
     public boolean canRenderOnFire() {
         return WorldUtil.shouldRenderFire((Entity) (Object) this);
     }
