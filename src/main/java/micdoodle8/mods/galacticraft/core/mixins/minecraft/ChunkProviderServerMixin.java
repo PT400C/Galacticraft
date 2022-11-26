c7b15ec4e1dc7577af7cb89c62ad37e09f637875
kuba6000 <kuba.123123.6000@gmail.com>
2022-11-26 22:01:13 +0100

    Quick fix

--- src/main/java/micdoodle8/mods/galacticraft/core/mixins/minecraft/ChunkProviderServerMixin.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/mixins/minecraft/ChunkProviderServerMixin.java
@@ -18,7 +18,8 @@ public class ChunkProviderServerMixin {
                             target =
                                     "Lcpw/mods/fml/common/registry/GameRegistry;generateWorld(IILnet/minecraft/world/World;Lnet/minecraft/world/chunk/IChunkProvider;Lnet/minecraft/world/chunk/IChunkProvider;)V",
                             value = "INVOKE"),
-            method = "populate(Lnet/minecraft/world/chunk/IChunkProvider;II)V",
+            method = "func_73153_a(Lnet/minecraft/world/chunk/IChunkProvider;II)V", // populate
+            remap = false,
             require = 1)
     private boolean checkOtherModPreventGenerate(
             int chunkX, int chunkZ, World world, IChunkProvider chunkProvider, IChunkProvider chunkGenerator) {
